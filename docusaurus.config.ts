dd4012b153846a053b14ba124c67eee73e95d959
Michael Hart <michael.hart1994@gmail.com>
2024-01-17 16:08:36 -0800

    Update site to use docusaurus

---
+++ docusaurus.config.ts
@@ -0,0 +1,147 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Mike Likes Robots',
+  tagline: 'A place for tutorials and sharing information so everyone can build better robots!',
+  favicon: 'favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://mikelikesrobots.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'mikelikesrobots', // Usually your GitHub org/user name.
+  projectName: 'mikelikesrobots.github.io', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts'
+        },
+        blog: {
+          showReadingTime: true
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+        gtag: {
+          trackingID: "G-7X3VQWMTBV"
+        }
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Mike Likes Robots',
+      logo: {
+        alt: 'Mike Likes Robots',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'gettingStartedSidebar',
+          position: 'left',
+          label: 'Getting Started',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://community.aws/@mikelikesrobots',
+          label: 'AWS Community',
+          position: 'right',
+        },
+        {
+          href: 'https://www.youtube.com/channel/UCAdis20vFM97syzwG1YlacQ',
+          label: 'YouTube',
+          position: 'right',
+        },
+        {
+          href: 'https://www.linkedin.com/in/michael-hart-a7614262/',
+          label: 'LinkedIn',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/mikelikesrobots',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started Guide',
+              to: '/docs/getting-started/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              href: 'https://community.aws/@mikelikesrobots',
+              label: 'AWS Community',
+            },
+            {
+              href: 'https://www.youtube.com/channel/UCAdis20vFM97syzwG1YlacQ',
+              label: 'YouTube',
+            },
+            {
+              href: 'https://www.linkedin.com/in/michael-hart-a7614262/',
+              label: 'LinkedIn',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'Flaticons - Freepik',
+              to: 'https://www.flaticon.com/free-icons/start-button',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Mike Likes Robots. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/components/GuideItem/index.tsx
@@ -0,0 +1,77 @@
+import styles from './styles.module.css';
+import Heading from '@theme/Heading';
+import Details from '@theme/Details';
+import Image from '@theme/ThemedImage';
+import Link from '@docusaurus/Link';
+
+export type GuideItemProps = {
+  title: string;
+  summary?: string;
+  youtube?: string;
+  local_blog?: string;
+  aws_community?: string;
+}
+
+function Summary(props: Pick<GuideItemProps, 'summary'>): JSX.Element {
+  return props.summary ?
+    <p>{props.summary}</p> :
+    null;
+}
+
+function YoutubeVideo(props: Pick<GuideItemProps, 'youtube'>): JSX.Element {
+  return props.youtube ?
+    <Details summary="Watch on YouTube!">
+      <iframe className="youtube-video" src={props.youtube} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </Details> :
+    null;
+}
+
+function LocalBlogLink(props: {href?: string}): JSX.Element {
+  return props.href ? 
+    <div className={styles.blog_link}>
+      <Link to={props.href}>
+        <Image height={"30px"} className={styles.pad_right} sources={{light: "/img/logo.svg", dark: "/img/logo.svg"}}/>
+        Read more on Mike Likes Robots Blog!
+      </Link>
+    </div> :
+    null;
+}
+
+function AWSCommunityBlogLink(props: {href?: string}): JSX.Element {
+  return props.href ? 
+    <div className={styles.blog_link}>
+      <Link to={props.href}>
+        <Image height={"30px"} className={styles.pad_right} sources={{light: "/img/aws_logo_smile_1200x630.png", dark: "/img/aws_logo_smile_1200x630.png"}}/>
+        Read more on AWS Community!
+      </Link>
+    </div> :
+    null;
+}
+
+function Blogs(props: Pick<GuideItemProps, 'local_blog' | 'aws_community'>): JSX.Element {
+  const local = <LocalBlogLink href={props.local_blog} />;
+  const aws = <AWSCommunityBlogLink href={props.aws_community} />;
+  if (local !== null || aws !== null) {
+    return <div className={styles.blog_links_wrapper}>
+      {local}
+      {aws}
+    </div>
+  } else {
+    return null;
+  }
+}
+
+export default function GuideItem(props: GuideItemProps): JSX.Element {
+  return (
+    <section className={styles.card}>
+      <div className={styles.card_title}>
+        <h5 className={styles.h5}>{props.title}</h5>
+      </div>
+      <div className={styles.card_container}>
+        <Summary {...props} />
+        <Blogs {...props} />
+        <YoutubeVideo {...props} />
+      </div>
+    </section>
+  )
+}
