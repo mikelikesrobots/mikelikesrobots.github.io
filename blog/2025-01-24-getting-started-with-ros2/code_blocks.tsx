4ef8450f7f023c7bed59827b479048013f644331
mikelikesrobots <mikelikesrobots@outlook.com>
2025-01-24 14:14:29 +0000

    Add Getting Started with ROS 2 (#36)

---
+++ blog/2025-01-24-getting-started-with-ros2/code_blocks.tsx
@@ -0,0 +1,9 @@
+export function Request() {
+  return <b style={{color: "red"}}>[Request ]</b>
+}
+export function Feedback() {
+  return <b style={{color: "green"}}>[Feedback]</b>
+}
+export function Response() {
+  return <b style={{color: "#4298f5"}}>[Response]</b>
+}
